86650d60b01a3deead0ceb69cae0cf7744ffcda9
Zizwar <zizwar@gmail.com>
2022-11-26 05:05:52 +0000

    add event ondblclick to knobs é clean code

--- wino/nexus.ts
+++ wino/nexus.ts
@@ -1,54 +1,58 @@
 export const nexusIno = () => {
-  const dialsArr = ["blue", "green", "orange", "white"];
+  const knobs = ["blue", "green", "orange", "white"];
 
-  dialsArr.forEach((item) =>
-    new Nexus.Dial("#imprint-" + item).on("change", function (value) {
-      document.querySelector("#display-circle-volume")
-        .setAttribute("cy", 100 - (value * 100));
+  const Knobs = knobs.map((knob) => {
+    const elmKnob = document.querySelector(`#imprint-${knob}`);
 
-      /*    document.querySelector("#display-rect-volume")
-        .setAttribute("y", 100 - (value * 100));
-      */
-      document.querySelector(".imprint-" + item).style.transform = `rotate(${
-        value * 360
-      }deg)`;
-    })
-  );
+    const ondblclick = (arg) => alert(arg);
+    elmKnob.ondblclick = () => ondblclick(knob);
 
-  const piano = new Nexus.Piano("#keyboard", {
+    return ({
+      [knob]: new Nexus.Dial(`#imprint-${knob}`).on("change", (value) => {
+        document.querySelector("#display-circle-volume")
+          .setAttribute("cy", 90 - (value * 90));
+        document.querySelector(`.imprint-${knob}`).style.transform = `rotate(${
+          value * 360
+        }deg)`;
+      }),
+    });
+  });
+
+  const Piano = new Nexus.Piano("#keyboard", {
     "size": [1024, 180],
     "mode": "button", // 'button', 'toggle', or 'impulse'
     "lowNote": 29,
     "highNote": 53,
   });
-  ////////
 
-  const controls_arr = "mixer,t1,t2,t3,t4,s1,s2,s3,s4,s5,s6,s7,s8,sequencer"
+  //
+  const controls = "mixer,t1,t2,t3,t4,s1,s2,s3,s4,s5,s6,s7,s8,sequencer"
     .split(
       ",",
     );
 
-  const elmControls = document.querySelectorAll(".controls .button-block");
+  document.querySelectorAll(".controls .button-block").forEach((
+    control,
+    index,
+  ) => (
+    control.id = controls[index]
+  ));
 
-  elmControls.forEach((control, index) => {
-    control.id = controls_arr[index];
-  });
-
-  const Controles = [];
-  elmControls.forEach((_, index) => {
-    const idElement = controls_arr[index];
-    const button = new Nexus.Add.Button(idElement, {
+  const Controles = controls.map((control) => {
+    const button = new Nexus.Add.Button(control, {
       "size": [44, 44],
       "mode": "button",
       "state": false,
     });
     button.parent.style.position = "absolute";
-    button.parent.style.opacity = 0.5;
-    Controles[idElement] = button;
+    button.parent.style.opacity = 0.12345;
+    return { [control]: button };
   });
   setTimeout(() => {
-    Controles.sequencer.parent.style.opacity = 1;
+    Knobs.orange.value = 0.2;
+    Knobs.blue.max = 0.5;
+    knobs.orange.value = 50;
+  }, 5000);
 
-  },5000)
- 
+  return ({ Controles, Piano, Knobs });
 };
